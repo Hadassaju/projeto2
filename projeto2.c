d30ea218e913187f0d4209c52bcf27e9e25b6d03
Hadassaju <138730887+Hadassaju@users.noreply.github.com>
2023-09-15 11:14:40 -0300

    Update soma vetor projeto2.c

--- projeto2.c
+++ projeto2.c
@@ -1,21 +1,44 @@
-#include <stdio.h>
+#include <stdio.h> // Para as funções de entrada e saída padrão
+
+char volatile *iostatus = (char *)0x0;
+char volatile *iodata = (char *)0xC;
+
+void int_to_string(unsigned short num, char str[]) {
+  // ... (código da função int_to_string igual ao seu exemplo)
+}
+
+void uart_send_string(char *str) {
+  // ... (código da função uart_send_string igual ao seu exemplo)
+}
+
+void uart_send_integer(unsigned short num) {
+  // ... (código da função uart_send_integer igual ao seu exemplo)
+}
+
+void sum_array(unsigned short *array, unsigned short size) {
+  unsigned int sum = 0;
+
+  for (unsigned short i = 0; i < size; i++) {
+    sum += array[i];
+  }
+
+  char sum_str[20];
+  int_to_string(sum, sum_str);
+
+  uart_send_string("Soma dos elementos do vetor: ");
+  uart_send_string(sum_str);
+  uart_send_string("\n");
+}
 
 int main() {
-    // Declaração de variáveis
-    float tensao, corrente, resistencia;
+  unsigned short array[] = {7, 2, 9, 4, 8, 15, 1};
+  unsigned short size = sizeof(array) / sizeof(array[0]);
 
-    // Solicitar ao usuário que insira a corrente e a resistência
-    printf("Digite a corrente (em Amperes): ");
-    scanf("%f", &corrente);
+  uart_send_string("\n\nSoma dos Elementos de um Vetor\n");
 
-    printf("Digite a resistência (em Ohms): ");
-    scanf("%f", &resistencia);
+  sum_array(array, size);
 
-    // Calcular a tensão usando a fórmula de Ohm
-    tensao = corrente * resistencia;
+  uart_send_string("\nFim.");
 
-    // Exibir o resultado
-    printf("A tensão é %.2f Volts\n", tensao);
-
-    return 0;
+  return 0;
 }
